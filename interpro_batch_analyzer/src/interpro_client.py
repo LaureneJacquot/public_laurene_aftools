f1a4eea086c92cfe6d0283cf543f21209e459a79
Laurène <laurenejacquot@gmail.com>
2025-09-30 19:27:58 +0200

    Update interpro_client.py

--- interpro_batch_analyzer/src/interpro_client.py
+++ interpro_batch_analyzer/src/interpro_client.py
@@ -47,7 +47,6 @@ class InterproClient :
                 json.dump(init, f, indent=2) 
 
         
-        #cookie_string = '_ga_11XLYWT8G7=GS2.3.s1747653692$o1$g0$t1747653692$j0$l0$h0; embl-ebi-public-website-v1.0-data-protection-accepted=true; _ga=GA1.1.1874504441.1744713762; _ga_F8566CJNX5=GS2.1.s1748956976$o2$g0$t1748956976$j60$l0$h0; _ga_LMNK5MEQ4Z=GS2.1.s1750345118$o12$g0$t1750345118$j60$l0$h0; _hjSessionUser_4953782=eyJpZCI6ImQ5MzhmMTEyLTQ5NGUtNTFiYS04ZTFkLWJiMjZjMWVlNGZmNSIsImNyZWF0ZWQiOjE3NDQ3MTM3NjA4NjMsImV4aXN0aW5nIjp0cnVlfQ==; _clck=qiw7uk%7C2%7Cfx3%7C0%7C1996; _ga_RPJQZYPKWN=GS2.1.s1750924706$o5$g1$t1750924903$j60$l0$h0; _ga_JD9QTGRJ05=GS2.1.s1751564761$o5$g1$t1751565255$j59$l0$h0'
         cookie_string = INTERPRO_COOKIES
 
 
